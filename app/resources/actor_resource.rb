5c76c93e76c87346b837e625f84f941b78cb3019
anonymous anonymous <tobias.wyler@kellogg.northwestern.edu>
2022-02-20 19:47:01 +0000

    Direct association between ActorResource and RoleResource

--- app/resources/actor_resource.rb
+++ app/resources/actor_resource.rb
@@ -8,6 +8,8 @@ class ActorResource < ApplicationResource
 
   # Direct associations
 
+  has_many   :roles
+
   # Indirect associations
 
 end
--- app/resources/role_resource.rb
+++ app/resources/role_resource.rb
@@ -7,6 +7,8 @@ class RoleResource < ApplicationResource
 
   # Direct associations
 
+  belongs_to :actor
+
   belongs_to :movie
 
   # Indirect associations

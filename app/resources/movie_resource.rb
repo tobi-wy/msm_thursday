95e477dfdd003921b9b015841c8de543968c5d31
anonymous anonymous <tobias.wyler@kellogg.northwestern.edu>
2022-02-20 19:46:56 +0000

    Generate graphiti resources for movies

---
+++ app/resources/movie_resource.rb
@@ -0,0 +1,14 @@
+class MovieResource < ApplicationResource
+  attribute :id, :integer, writable: false
+  attribute :created_at, :datetime, writable: false
+  attribute :updated_at, :datetime, writable: false
+  attribute :title, :string
+  attribute :year, :integer
+  attribute :description, :string
+  attribute :director_id, :integer
+
+  # Direct associations
+
+  # Indirect associations
+
+end

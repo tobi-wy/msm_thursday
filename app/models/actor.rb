0e0fbbdb0a3d1515462c1e357a73089ac311bccc
anonymous anonymous <tobias.wyler@kellogg.northwestern.edu>
2022-02-20 19:46:24 +0000

    Indirect association between movies and actors

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -6,6 +6,10 @@ class Actor < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :movies,
+             :through => :roles,
+             :source => :movie
+
   # Validations
 
   # Scopes
--- app/models/movie.rb
+++ app/models/movie.rb
@@ -9,6 +9,10 @@ class Movie < ApplicationRecord
 
   # Indirect associations
 
+  has_many   :actors,
+             :through => :roles,
+             :source => :actor
+
   # Validations
 
   # Scopes
